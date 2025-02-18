4689916a5f41406cb6d57a3802fad0d98ac82c7c
Vishrut Kathwal <vishrut.py@gmail.com>
2025-02-18 10:42:14 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -4,22 +4,16 @@ int main() {
     int marks;
     scanf("%d", &marks);
 
-    switch (marks) {
-        case (marks>=90):
-            printf("A");
-            break;
-        case (marks>=80 && marks<90):
-            printf("B");
-            break;
-        case (marks>=70 && marks<80):
-            printf("C");
-            break;
-        case (marks>=60 && marks<70):
-            printf("D");
-            break;
-        case (marks<60):
-            printf("F");
-            break;
+    if (marks >= 90) {
+        printf("A");
+    } else if (marks >= 80) {
+        printf("B");
+    } else if (marks >= 70) {
+        printf("C");
+    } else if (marks >= 60) {
+        printf("D");
+    } else {
+        printf("F");
     }
 
     return 0;
