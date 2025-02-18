b86439f182eeea73b66be7cc8425d2029fac5106
Vishrut Kathwal <vishrut.py@gmail.com>
2025-02-18 10:34:33 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,10 +5,10 @@ int main(){
     if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
         printf("Vowel");
     }
-    else if(a>='a' && a<=z) {
+    else if(a>='a' && a<='z') {
         printf("Consonant");
     }
-    else if (a>='0' && a<='9'){
+    else if (a>=0 && a<=9){
         printf("Digit");
     }
     else{
