851a454806ff076cc97e638579c6628dd9b32f9c
Vishrut Kathwal <vishrut.py@gmail.com>
2025-02-18 13:09:13 +0530

    Solution to Check Voting Eligibility by Age and Citizenship

---
+++ Check Voting Eligibility by Age and Citizenship@@6788dd63490637cfc44e1ba5/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, eligible = 1;
+    scanf("%d", &num);
+
+    if (num < 18) {
+        eligible = 0;  // Numbers less than or equal to 1 are not prime
+    } else {
+        isPrime = 0;  // Found a divisor, not a prime number
+            }
+
+    if (isPrime == 1) {
+        printf("Eligible");
+    } else {
+        printf("Not Eligible");
+    }
+
+    return 0;
+}
