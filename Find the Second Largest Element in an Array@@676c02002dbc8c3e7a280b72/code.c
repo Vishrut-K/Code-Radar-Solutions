1e3852b1e33169bb405bedf192fa43c6dea5330f
Vishrut Kathwal <vishrut.py@gmail.com>
2025-04-09 19:09:03 +0530

    Solution to Find the Second Largest Element in an Array

---
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Edge case: If there is only one element
+    if (n < 2) {
+        printf("-1");
+        return 0;
+    }
+
+    // Initialize largest and second largest
+    int largest = arr[0], sec = -1000000;
+
+    // Finding largest and second largest
+    for (int i = 1; i < n; i++) { 
+        if (arr[i] > largest) {
+            sec = largest;
+            largest = arr[i];
+        } else if (arr[i] > sec && arr[i] < largest) {
+            sec = arr[i];
+        }
+    }
+
+    // If second largest is still -1, it means all elements were the same
+    if (sec == -1000000) {
+        printf("-1");
+    } else {
+        printf("%d\n", sec);
+    }
+
+    return 0;
+}
